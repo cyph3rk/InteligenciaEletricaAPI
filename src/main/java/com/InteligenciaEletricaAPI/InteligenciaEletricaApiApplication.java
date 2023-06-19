f884fbd1330a50ca342cce2d9f6271f3b609cf2e
cyph3rk <cyph3rk@gmail.com>
2023-06-18 21:04:50 -0300

    Adicionado SpringBoot Web

---
+++ src/main/java/com/InteligenciaEletricaAPI/InteligenciaEletricaApiApplication.java
@@ -0,0 +1,13 @@
+package com.InteligenciaEletricaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteligenciaEletricaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteligenciaEletricaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InteligenciaEletricaAPI/InteligenciaEletricaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InteligenciaEletricaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteligenciaEletricaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
